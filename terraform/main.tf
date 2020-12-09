c8897aa29805eb4d19f2888f4f561b7ca17f9b10
kpmg-arul-test <arulkr1967@gmail.com>
2020-12-09 00:45:26 +0000

    Revert "first commit"
    This reverts commit 3a61b0beb32f03ad394d7ce58e37218dd8ed7483.

--- terraform/main.tf
+++
@@ -1,8 +0,0 @@
-#
-# Google Cloud Platform
-#
-provider "google" {
-  project = var.project
-  region  = var.region
-  #credentials = "/home/arulkr1967/terraform_sa.json"
-}
--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-#
-# Output for K8S
-
-output "host" {
-  value = google_container_cluster.cluster.endpoint
-  sensitive = false
-}
